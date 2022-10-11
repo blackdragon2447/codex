bb68424d4bcfce3385f47e36d2b27ca261f89f0d
BlackDragon2447 <blackdragon2447@e.email>
2022-10-11 12:20:00 +0200

    Attempt at out own database wrapper

--- app/src/main/java/com/blackdragon2447/codex/database/Querry.java
+++ app/src/main/java/com/blackdragon2447/codex/database/Querry.java
@@ -6,20 +6,25 @@ import java.util.stream.Collectors;
 public interface Querry {
   
   public static class Select implements Querry {
-   
+
     private ArrayList<QuerryToken> tokens = new ArrayList<>();
 
-    public Select(String selector, String table){
-      this.tokens.add(new QuerryToken.Select(selector)); 
+    public Select(String selector, String table) {
+      this.tokens.add(new QuerryToken.Select(selector));
       this.tokens.add(new QuerryToken.From(table));
     }
 
-    public String assemble() {
-      
-      return tokens.stream().map((token) -> token.assemble()).collect(Collectors.joining(" "));
-
+    public Select whereEqual(String column, String value) {
+      this.tokens.add(QuerryToken.Where.equals(column, value));
+      return this;
     }
 
+    public String assemble() {
+
+      return tokens.stream()
+          .map((token) -> token.assemble())
+          .collect(Collectors.joining(" "));
+    }
   }
 
   abstract String assemble();
@@ -27,46 +32,55 @@ public interface Querry {
   interface QuerryToken {
 
     public class Select implements QuerryToken {
-      
+
       private final String selector;
 
-      protected Select(String selector){
-        this.selector = selector;
-      }
+      protected Select(String selector) { this.selector = selector; }
 
-      public String assemble() {
-        return "SELECT " + selector;
-      }
-
+      public String assemble() { return "SELECT " + selector; }
     }
 
     public class From implements QuerryToken {
 
       private final String table;
 
-      protected From(String table) {
-        this.table = table;
-      }
+      protected From(String table) { this.table = table; }
 
-      public String assemble() {
-        return "FROM " + table;
-      }
-
+      public String assemble() { return "FROM " + table; }
     }
 
     public class Where implements QuerryToken {
 
+      private final Type type;
+      private final String column;
+      private final String value;
 
-      public String assemble() {
-        
+      public static Where equals(String column, String value){
+        return new Where(Type.EQ, column, value);
+      }
 
-        return "";
+      private Where(Type type, String column, String value){
+        this.type = type;
+        this.column = column;
+        this.value = value;
+      }
+
+      public String assemble() { return ""; }
+
+      public enum Type {
+        EQ,
+        NE,
+        LT,
+        LE,
+        GT,
+        GE,
+        IN,
+        LIKE,
+        BETWEEN;
       }
 
     }
 
     abstract String assemble();
-
   }
-
 }
